88d6436c3eb2596b381a50648181d2bfe85c047a
Wei Li <weili.mail@gmail.com>
2025-12-14 06:46:28 -0500

    Use custom restclient auto configuration library

--- src/main/java/com/example/demo/config/restclient/ServiceClientAuthProperties.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.config.restclient;
-
-import java.util.HashMap;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "serviceclient")
-public class ServiceClientAuthProperties extends HashMap<String, ClientAuthProperties> {}
